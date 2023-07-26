e2341514961cead2626f3debe04b82b51aec8b84
nicolas <nico.age@hotmail.com>
2023-07-26 00:11:25 -0200

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Router";
+import { router } from "./router";
 import "./App.css";
 import { CardProvider } from "./useContext";
 
